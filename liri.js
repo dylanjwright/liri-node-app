1566d89f95769bd58137bb0ac4ecfd2765aa17cd
Dylan Wright <wright.dylan5@gmail.com>
2019-04-24 19:42:23 -0400

    added spotify api and command

--- liri.js
+++ liri.js
@@ -7,6 +7,7 @@ var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var query = process.argv[3];
 
+//------SPOTIFY------//
 
 var spotThis = function(songName) {
 
@@ -22,17 +23,27 @@ var spotThis = function(songName) {
         if (err) {
             return console.log('Error occurred: ' + err);
         } 
-        else { for (var i = 0; i < data.tracks.items[0].artists[i].length; i++) ;
+        else { for (var i = 0; i < data.tracks.items[0].artists[i].length; i++);
 
         console.log("\nArtist(s): " + data.tracks.items[0].artists[i].name)
         console.log("Song: " + data.tracks.items[0].name); 
         console.log("Preview " + data.tracks.items[0].preview_url); 
         console.log("Album: " + data.tracks.items[0].album.name);
         console.log("\n"); 
-
+        
         }
     }
-    )};
+)};
+
+
+
+
+
+
+
+
+
+//------COMMANDS------//
 
 if(command === "spotify-this-song") {
     spotThis(query);
