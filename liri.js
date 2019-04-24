135baa35dc05154c0ed5a871162332be5a76909e
Dylan Wright <wright.dylan5@gmail.com>
2019-04-24 19:40:56 -0400

    added spotify api and command

--- liri.js
+++ liri.js
@@ -4,10 +4,36 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+var command = process.argv[2];
+var query = process.argv[3];
+
+
+var spotThis = function(songName) {
+
+    if(songName === undefined) {
+        songName = "the sign ace of base";
+    };
+
+    spotify.search({
+        type: 'track',
+        query: songName },
+        function(err, data) {
+
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } 
+        else { for (var i = 0; i < data.tracks.items[0].artists[i].length; i++) ;
+
+        console.log("\nArtist(s): " + data.tracks.items[0].artists[i].name)
+        console.log("Song: " + data.tracks.items[0].name); 
+        console.log("Preview " + data.tracks.items[0].preview_url); 
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("\n"); 
+
+        }
     }
-   
-  console.log(data.tracks.items[0]); 
-  });
+    )};
+
+if(command === "spotify-this-song") {
+    spotThis(query);
+}
